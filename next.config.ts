9623672f67d96ce41321f9f03e614b84f22c52f5
rkritik0110 <122217699+rkritik0110@users.noreply.github.com>
2025-06-07 15:28:09 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,34 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Exclude 'async_hooks' from client-side bundles
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        async_hooks: false,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
